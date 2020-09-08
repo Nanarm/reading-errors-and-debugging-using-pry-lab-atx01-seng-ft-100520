0d858d54d30e56a1064e17050267867a5535ace1
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-08 16:31:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,11 +7,11 @@ def generate_star_date
 end
 
 
-def state_log(date)
+def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
   puts state_log(generate_star_date)
-  date = generate_star_date
+  star_date = generate_star_date
 end
